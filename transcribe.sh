16492b11979e732aa2b39308e7a38c93e5d9709a
root@LocalGhost <rootlocalghost@gmail.com>
2026-02-13 10:21:04 +0600

    Update transcribe.sh

--- transcribe.sh
+++ transcribe.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 
 # Example: Transcribe the song we just generated
 # Change the filename below to match your generated file
-SONG_PATH="./output/echoes_of_silence.mp3"
+SONG_PATH="./output/song.mp3"
 
 python xpu_transcribe.py \
     --music_path "$SONG_PATH" \
